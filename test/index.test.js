815c30ef2b40d3fc90bebfc6b093581f35962b93
Jordan Antoniou <jordan.antoniou@bbc.co.uk>
2020-12-08 11:23:06 +0000

    two sum and multiply feature

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,12 @@
 import index from '../src/index';
+import twoSumAndMultiplyFixture from './fixtures/twoSumAndMultiplyFixture';
 
 describe('index', () => {
-    it('should return true', () => {
-        expect(index.day1()).toBe(true);
+    it('should multiply two elements which sum to target value', () => {
+        const numbers = twoSumAndMultiplyFixture;
+        const target = 2020;
+        const expectedAnswer = 1009899;
+
+        expect(index.twoSumAndMultiply(numbers, target)).toBe(expectedAnswer);
     });
 });
